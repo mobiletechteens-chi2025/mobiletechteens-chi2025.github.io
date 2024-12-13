952142869d8bdc8eb0066e85c28e7e4eae73d4dd
greenina <igreen0485@gmail.com>
2024-12-13 12:34:33 +0900

    Update href

--- scripts/responsive.js
+++
@@ -1,9 +0,0 @@
-// menu-toggle.js
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('hidden'); // Show or hide the menu
-  menu.classList.toggle('flex'); // Apply flex display
-  menu.classList.toggle('flex-col'); // Ensure vertical alignment
-});
